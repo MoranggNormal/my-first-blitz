1a6e93eef58bffa06db0a56a9670a5cbb449a987
Euller Peixoto <peixotoeuller500@gmail.com>
2022-01-21 23:11:01 -0200

    test

--- app/core/components/LabeledTextField.js
+++ app/core/components/LabeledTextField.js
@@ -1,38 +1,35 @@
-import { forwardRef } from "react";
-import { useField } from "react-final-form";
-export const LabeledTextField = forwardRef(({
-  name,
-  label,
-  outerProps,
-  fieldProps,
-  labelProps,
-  ...props
-}, ref) => {
-  const {
-    input,
-    meta: {
-      touched,
-      error,
-      submitError,
-      submitting
-    }
-  } = useField(name, {
-    parse: props.type === "number" ? Number : // Converting `""` to `null` ensures empty values will be set to null in the DB
-    v => v === "" ? null : v,
-    ...fieldProps
-  });
-  const normalizedError = Array.isArray(error) ? error.join(", ") : error || submitError;
-  return <div {...outerProps}>
+import { forwardRef } from "react"
+import { useField } from "react-final-form"
+export const LabeledTextField = forwardRef(
+  ({ name, label, outerProps, fieldProps, labelProps, ...props }, ref) => {
+    const {
+      input,
+      meta: { touched, error, submitError, submitting },
+    } = useField(name, {
+      parse:
+        props.type === "number"
+          ? Number // Converting `""` to `null` ensures empty values will be set to null in the DB
+          : (v) => (v === "" ? null : v),
+      ...fieldProps,
+    })
+    const normalizedError = Array.isArray(error) ? error.join(", ") : error || submitError
+    return (
+      <div {...outerProps}>
         <label {...labelProps}>
           {label}
           <input {...input} disabled={submitting} {...props} ref={ref} />
         </label>
 
-        {touched && normalizedError && <div role="alert" style={{
-      color: "red"
-    }}>
+        {touched && normalizedError && (
+          <div
+            role="alert"
+            style={{
+              color: "red",
+            }}
+          >
             {normalizedError}
-          </div>}
+          </div>
+        )}
 
         <style jsx>{`
           label {
@@ -42,7 +39,7 @@ export const LabeledTextField = forwardRef(({
             font-size: 1rem;
           }
           input {
-            font-size: 1rem;
+            font-size: 1.1rem;
             padding: 0.25rem 0.5rem;
             border-radius: 3px;
             border: 1px solid purple;
@@ -50,6 +47,8 @@ export const LabeledTextField = forwardRef(({
             margin-top: 0.5rem;
           }
         `}</style>
-      </div>;
-});
-export default LabeledTextField;
+      </div>
+    )
+  }
+)
+export default LabeledTextField
